6ed25954798c884f400fc524497ac1f6c7c88319
firkax-new-desk <157710697+Firkax69@users.noreply.github.com>
2024-07-24 11:54:55 +0100

    feat: add Form and Preview and some css

---
+++ src/Components/CVPreview/PreviewPersonal.js
@@ -0,0 +1,14 @@
+import "./style.css";
+export default function PreviewPersonal({firstName, lastName, title, address, email, phoneNumber, description}) {
+    return (
+        <div id="preview-personal">
+            <p>{firstName}</p>
+            <p>{lastName}</p>
+            <p>{title}</p>
+            <p>{address}</p>
+            <p>{email}</p>
+            <p>{phoneNumber}</p>
+            <p>{description}</p>
+        </div>
+    )
+}
